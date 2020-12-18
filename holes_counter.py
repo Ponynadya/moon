4dec76bdac9f8af1399e288f7bce01c258da63db
Ponynadya <zaharovanadya24@gmail.com>
2020-12-18 14:23:45 +0300

    Fixes

--- holes_counter.py
+++ holes_counter.py
@@ -1,22 +1,10 @@
-hole = 1
+def calculate(moon_map) -> list:
+    """
 
-file_name = input('Введите название файла: ')
-with open(file_name + '.txt', 'r') as f:
-    file_data = f.read()
-    raw_moon_data = file_data.split('\n')
-    raw_moon_data.pop()
-
-
-moon = []
-for _str in raw_moon_data:
-    row = []
-    _str = _str.split()
-    for symbol in _str:
-        row.append(int(symbol))
-    moon.append(row)
-
-
-def calculate(moon_map):
+    :param moon_map: 
+    :return: 
+    """
+    hole = 1
 
     to_check = []
     global_holes = 0
@@ -24,12 +12,15 @@ def calculate(moon_map):
     for y, y_value in enumerate(moon_map):
         for x, x_value in enumerate(y_value):
             if moon_map[y][x] == hole:
+                a = moon_map[y][x]
                 to_check.append((y, x))
 
                 while to_check:
                     # find near
                     target = to_check.pop()
                     for y_step in range(-1, 2):
+                        if target[0] + y_step < 0:
+                            continue
                         try:
                             if moon_map[target[0] + y_step][target[1]] == hole:
                                 to_check.append((target[0] + y_step, target[1]))
@@ -38,6 +29,8 @@ def calculate(moon_map):
                             continue
 
                     for x_step in range(-1, 2):
+                        if target[1] + x_step < 0:
+                            continue
                         try:
                             if moon_map[target[0]][target[1] + x_step] == hole:
                                 to_check.append((target[0], target[1] + x_step))
@@ -52,10 +45,27 @@ def calculate(moon_map):
     return global_holes
 
 
-print('MOON MAP:')
-for y in moon:
-    for x in y:
-        print(x, ' ', end='')
-    print()
+if __name__ == "__main__":
 
-print('Quantity of holes: ', calculate(moon))
+    file_name = input('Введите название файла: ')
+    with open(file_name + '.txt', 'r') as f:
+        file_data = f.read()
+        raw_moon_data = file_data.split('\n')
+        raw_moon_data.pop()
+
+
+    moon = []
+    for _str in raw_moon_data:
+        row = []
+        _str = _str.split()
+        for symbol in _str:
+            row.append(int(symbol))
+        moon.append(row)
+
+    print('MOON MAP:')
+    for y in moon:
+        for x in y:
+            print(x, ' ', end='')
+        print()
+
+    print('Quantity of holes: ', calculate(moon))
